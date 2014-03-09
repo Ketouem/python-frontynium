6849c3b2d476a3dede4e5a0fda74a9e5e61916de
Ketouem <ketouem@gmail.com>
2014-03-10 00:43:33 +0100

    implemented gettable & settable generator

--- frontynium/page.py
+++ frontynium/page.py
@@ -8,7 +8,7 @@ class Page(object):
     def __init__(self, root_node):
         """Default constructor
 
-        root_node -- instance of selenium's webdriver or webelement
+        :param root_node: instance of selenium's webdriver or webelement
         """
         self._root_node = root_node
         self._finder = Finder(self._root_node)
@@ -20,10 +20,15 @@ class Page(object):
     #Preventing a var = Page(webdriver) in a test code as _objects needs to be defined correctly
     def __new__(cls, *args, **kwargs):
         if cls is Page:
-            raise TypeError("This class is not meant to be instantiated directly but subclassed")
+            raise TypeError("This class is not meant to be instantiated directly but subclassed.")
         return object.__new__(cls, *args, **kwargs)
 
-    def _find_objects(self, object_name, *args, **kwargs):
+    def detect_objects(self, object_name, *args, **kwargs):
+        """Trigger the detection of objects
+
+        :param object_name: the name of the mapping as defined in self._objects
+        :returns WebElement or list<WebElement>
+        """
         element = None
         try:
             mapping = self._objects[object_name].build()
@@ -34,8 +39,14 @@ class Page(object):
         return element
 
     def click_on(self, object_name, *args, **kwargs):
-        element = self._find_objects(object_name, *args, **kwargs)
+        """Click on an object defined in _objects
+
+        :param object_name: the name of the mapping as defined in self._objects
+        :returns the current instance of Page
+        """
+        element = self.detect_objects(object_name, *args, **kwargs)
         element.click()
+        return self
 
     @property
     def root_node(self):
@@ -49,11 +60,29 @@ class Page(object):
     def objects(self, value):
         self._objects = value
 
-def gettable():
-    pass
 
-def settable():
-    pass
+def gettable(*args):
+    def inner(cls):
+        for arg in args:
+            def getter(cls, *sargs, **skwargs):
+                return cls.detect_objects(arg, *sargs, **skwargs)
+            setattr(cls, 'get_' + arg, getter)
+        return cls
+    return inner
+
+
+def settable(*args):
+    def inner(cls):
+        for arg in args:
+            def setter(cls, value, clear_before_use=False, *sargs, **skwargs):
+                obj = cls.detect_objects(arg, *sargs, **skwargs)[0]
+                if clear_before_use:
+                    obj.clear()
+                obj.send_keys(value)
+                return cls
+            setattr(cls, 'set_' + arg, setter)
+        return cls
+    return inner
 
 def clickable():
     pass
